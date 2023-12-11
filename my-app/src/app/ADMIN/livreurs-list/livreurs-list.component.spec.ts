b78b97fa9a9b5184dd2e420bb84e11dec76f8ae6
Oussama <oussama.elbouhtani@student.vinci.be>
2023-12-11 19:07:35 +0100

    livreurs page

---
+++ my-app/src/app/ADMIN/livreurs-list/livreurs-list.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LivreursListComponent } from './livreurs-list.component';
+
+describe('LivreursListComponent', () => {
+  let component: LivreursListComponent;
+  let fixture: ComponentFixture<LivreursListComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LivreursListComponent]
+    });
+    fixture = TestBed.createComponent(LivreursListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
